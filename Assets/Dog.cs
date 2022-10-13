6ed03b991f9d80e8c8e399820d30f3138f46fd94
Han Jiang <thehanjiang@gmail.com>
2022-10-13 07:43:27 -0400

    Modify dog moving

--- Assets/Dog.cs
+++ Assets/Dog.cs
@@ -11,6 +11,12 @@ public class Dog : MonoBehaviour
     private int current;
     // Start is called before the first frame update
 
+    public bool StartMoving;
+
+    private void Start()
+    {
+        StartMoving = false;
+    }
 
     // Update is called once per frame
     void Update()
@@ -22,18 +28,28 @@ public class Dog : MonoBehaviour
             transform.position = pos;
         }
         else current = (current + 1) % target.Length;*/
-
-
-        if (transform.position != target[current].position)
+        if (StartMoving == true)
         {
-            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
-            //GetComponent<Rigidbody>().MovePosition(pos);
-            transform.position = pos;
-        }
-        else if (current < target.Length - 1)
-        {
-            current++;
+
+            if (transform.position != target[current].position)
+            {
+                Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+                //GetComponent<Rigidbody>().MovePosition(pos);
+                transform.position = pos;
+            }
+            else if (current < target.Length - 1)
+            {
+                current++;
+            }
         }
 
+    }
+
+    void OnTriggerStay(Collider target)
+    {
+        if (target.tag == "Player")
+        {
+            StartMoving = true;
+        }
     }
 }
